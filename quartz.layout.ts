c771b42544b31e9f245b415deebc0a719d1bddc5
4shen0ne <4shen.01@gmail.com>
2024-12-11 19:21:09 +0800

    custom config

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -3,7 +3,11 @@ import * as Component from "./quartz/components"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
+  head: Component.Head({
+    externalResources: {
+      css: ["https://cdn.jsdelivr.net/npm/@callmebill/lxgw-wenkai-web@latest/style.css"],
+    }
+  }),
   header: [],
   afterBody: [],
   footer: Component.Footer({
